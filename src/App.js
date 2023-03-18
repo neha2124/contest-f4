6e4f9baa8e1ce0708f81bb0bc23c8d04fbd28e88
neha2124 <nehashukla2124@gmail.com>
2023-03-19 02:43:38 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import { useState } from 'react';
+import './style.css';
+import Navbar from './navbar';
+import movies from './movieApi';
+import { genres } from './movieApi';
+import Movie from './movie'
+
 
 function App() {
+  const [movie, setMovie] = useState(movies)
+  const [genre] = useState(genres)
+  const filter = (index) =>{
+    let data =movies.filter((item) =>{
+      return item.genre === index
+    })
+    // console.log(data)
+    setMovie(data)
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div className='container'>
+      <Navbar data={genre} filter={filter}/>
+       <Movie data={movie}/>
     </div>
-  );
+);
 }
 
 export default App;
